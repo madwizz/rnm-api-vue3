29467174313d4320c31095fbd1ba621481352cc8
Dmitrii Sapegin <lejeboka24@gmail.com>
2023-10-06 15:16:29 +0200

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: 'https://madwizz.github.io/rnm-api-vue3/',
-})
